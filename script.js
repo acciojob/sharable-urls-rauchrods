8ba8c69379ebc4b9c34682f1814657217cc6a464
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-30 12:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+let button = document.querySelector("#button");
+let nameobj  = document.querySelector("#name");
+let yearobj  = document.querySelector("#year");
+let urlobj  = document.querySelector("#url");
+var form= document.querySelector("form");
+
+button.addEventListener("click",()=>{
+    let nametxt = nameobj.value;
+    let yeartxt = yearobj.value;
+    if(nametxt=="" && yeartxt==""){
+      urlobj.innerText = urlobj.innerText+  "yo";
+    }
+    else if(nametxt!="" && yeartxt==""){
+        urlobj.innerText = urlobj.innerText+  `?name=${nametxt}`;
+    }
+    else if(nametxt=="" && yeartxt!=""){
+        urlobj.innerText = urlobj.innerText+  `?year=${yeartxt}`;
+    }
+    else{
+        urlobj.innerText = urlobj.innerText+  `?name=${nametxt}&year=${yeartxt}`; 
+    }
+});
+
+form.addEventListener('submit', (e)=>{
+     e.preventDefault;
+});
